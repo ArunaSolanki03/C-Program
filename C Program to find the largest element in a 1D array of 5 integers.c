e0c6eb14473a81e85414d797bed6c16e6dbf5189
ArunaSolanki03 <arunasolanki305@gmail.com>
2025-11-29 18:47:27 +0530

    Add files via upload

---
+++ C Program to find the largest element in a 1D array of 5 integers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+ {
+    int arr[5] , i , largest;
+
+    printf("Enter 5 integers:\n");
+
+    for (i = 0; i < 5; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+
+    for (i = 1; i < 5; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
