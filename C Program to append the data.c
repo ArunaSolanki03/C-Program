851ef0ace2f8c637a588654dfd8aa1a7680cc0c5
ArunaSolanki03 <arunasolanki305@gmail.com>
2026-02-09 08:56:48 +0530

    Add files via upload

---
+++ C Program to append the data.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+FILE *fp;
+int ch;
+fp=fopen("data.txt","a");
+if(fp==NULL)
+{
+printf("File cannot be opened!\n");
+return 1;
+}
+fprintf(fp,"This line is appended at the end.\n");
+fclose(fp);
+printf("Data appended successfully.\n");
+return 0;
+}
